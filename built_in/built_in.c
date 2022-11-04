f65b4376f7a08754b73cc911289855b57c6818e1
Seubmarine <seubmarinepro@gmail.com>
2022-11-04 20:30:45 +0100

    add pwd built-in

--- built_in/built_in.c
+++ built_in/built_in.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:20:14 by tbousque          #+#    #+#             */
-/*   Updated: 2022/11/03 14:00:31 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/11/04 20:21:07 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 //return -1 on error, 0 when command is not a buitin and 1 on success
 int	builtin(char **argv, t_env *env)
 {
+	size_t	command_size;
+
+	command_size = strlen(argv[0]);
 	if (argv == NULL || argv[0] == NULL)
 		return (-1);
-	if (strncmp(argv[0], "export", 6) == 0)
+	if (strncmp(argv[0], "export", command_size) == 0)
 		return (builtin_export(argv, env));
-	else if (strncmp(argv[0], "unset", 6) == 0)
+	else if (strncmp(argv[0], "unset", command_size) == 0)
 		return (builtin_unset(argv, env));
-	else if (strncmp(argv[0], "env", 3) == 0)
+	else if (strncmp(argv[0], "env", command_size) == 0)
 		return (builtin_env(argv, env));
+	else if (strncmp(argv[0], "pwd", command_size) == 0)
+		return (builtin_pwd());
 	return (0);
 }
--- built_in/pwd.c
+++ built_in/pwd.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   built_in.h                                         :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/02 15:09:07 by tbousque          #+#    #+#             */
-/*   Updated: 2022/11/04 20:17:14 by tbousque         ###   ########.fr       */
+/*   Created: 2022/11/04 20:13:41 by tbousque          #+#    #+#             */
+/*   Updated: 2022/11/04 20:17:42 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILT_IN_H
-# define BUILT_IN_H
-# include <unistd.h>
-# include <stdio.h>
-# include "environement_variable.h"
+#include "built_in.h"
 
-int	builtin_unset(char **argv, t_env *env);
-int	builtin_export(char **argv, t_env *env);
-int builtin_env(char **argv, t_env *env);
-int builtin_pwd(void);
-int	builtin(char **argv, t_env *env);
-#endif
+int builtin_pwd(void)
+{
+	char	*current_directory;
+
+	current_directory = getcwd(NULL, 0);
+	if (current_directory == NULL)
+		return (-1);
+	printf("%s\n", current_directory);
+	free(current_directory);
+	return (1);
+}
