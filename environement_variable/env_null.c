348a090e87427feef86a18efc76eb2ec7829dbeb
tbousque <tbousque@student.42.fr>
2022-11-10 18:44:38 +0100

    fix empty env, forgot to set default

--- environement_variable/env_null.c
+++ environement_variable/env_null.c
@@ -11,5 +11,7 @@ t_env	env_init_null(char *argv)
 	free(str);
 	env_set_var(&env, "SHLVL", "1");
 	env_set_var(&env, "_", argv);
+	env._last_status_str = malloc(sizeof(ENV_LAST_STATUS_SIZE));
+	env_set_last_status(&env, 0);
 	return (env);
 }
