225126eefa16334e30dda56b604d1796b3117e54
Seubmarine <seubmarinepro@gmail.com>
2022-11-18 05:48:19 +0100

    norm lexer_helper.c

--- lexer/lexer_helper.c
+++ lexer/lexer_helper.c
@@ -6,16 +6,31 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 03:36:34 by tbousque          #+#    #+#             */
-/*   Updated: 2022/11/17 03:43:02 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/11/18 05:46:41 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
+t_token_info	is_token_string_or_space(char const *str)
+{
+	size_t			i;
+	t_token_info	next;
+
+	i = 0;
+	while (isspace(str[i]))
+			i++;
+	if (i > 0)
+		return ((t_token_info){.type = TOKEN_SPACE, .len = i});
+	next = is_token(&str[1]);
+	if (next.type == TOKEN_STRING)
+		return ((t_token_info){.type = TOKEN_STRING, .len = 1 + next.len});
+	return ((t_token_info){.type = TOKEN_STRING, .len = 1});
+}
+
 t_token_info	is_token(char const *str)
 {
 	size_t								i;
-	t_token_info						next;
 	static const t_token_parse_helper	parse_helper[] = {
 	{.value = ">>", .type = TOKEN_REDIRECT_OUTPUT_APPEND},
 	{.value = ">", .type = TOKEN_REDIRECT_OUTPUT},
@@ -32,22 +47,13 @@ t_token_info	is_token(char const *str)
 		return ((t_token_info){.type = TOKEN_END, .len = 1});
 	while (i < (sizeof(parse_helper) / sizeof(parse_helper[0])))
 	{
-		if (ft_strncmp(parse_helper[i].value, str, strlen(parse_helper[i].value)) \
-		== 0)
+		if (ft_strncmp(parse_helper[i].value, str, \
+		strlen(parse_helper[i].value)) == 0)
 			return ((t_token_info){parse_helper[i].type, \
 			ft_strlen(parse_helper[i].value)});
 		i++;
 	}
-	//check TOKEN_SPACE && TOKEN_STRING
-	i = 0;
-	while (isspace(str[i]))
-			i++;
-	if (i > 0)
-		return ((t_token_info){.type = TOKEN_SPACE, .len = i});
-	next = is_token(&str[1]);
-	if (next.type == TOKEN_STRING)
-		return ((t_token_info){.type = TOKEN_STRING, .len = 1 + next.len});
-	return ((t_token_info){.type = TOKEN_STRING, .len = 1});
+	return (is_token_string_or_space(str));
 }
 
 void	token_free(t_token *token)
@@ -60,22 +66,11 @@ void	token_free(t_token *token)
 void	lexer_debug(t_vec	tokens)
 {
 	const char	*lex_token[] = {
-		"TOKEN_STRING",
-		"TOKEN_PIPE",
-		"TOKEN_OR",
-		"TOKEN_AND",
-		"TOKEN_ASTERISK",
-		"TOKEN_REDIRECT_INPUT",
-		"TOKEN_REDIRECT_OUTPUT",
-		"TOKEN_REDIRECT_OUTPUT_APPEND",
-		"TOKEN_HERE_DOCUMENT",
-		"TOKEN_HERE_DOCUMENT_OPEN",
-		"TOKEN_DOLLAR",
-		"TOKEN_SINGLE_QUOTE",
-		"TOKEN_DOUBLE_QUOTE",
-		"TOKEN_END",
-		"TOKEN_SPACE"
-		"TOKEN_TYPE_UNKNOW",
+		"TOKEN_STRING", "TOKEN_PIPE", "TOKEN_OR", "TOKEN_AND",
+		"TOKEN_ASTERISK", "TOKEN_REDIRECT_INPUT", "TOKEN_REDIRECT_OUTPUT",
+		"TOKEN_REDIRECT_OUTPUT_APPEND", "TOKEN_HERE_DOCUMENT",
+		"TOKEN_HERE_DOCUMENT_OPEN", "TOKEN_DOLLAR", "TOKEN_SINGLE_QUOTE",
+		"TOKEN_DOUBLE_QUOTE", "TOKEN_END", "TOKEN_SPACE" "TOKEN_TYPE_UNKNOW",
 	};
 	size_t		i;
 	t_token		current;
@@ -108,17 +103,12 @@ void	tokens_append(t_vec *tokens, t_token *current)
 		}
 		else if (last->type == TOKEN_STRING && current->type == TOKEN_STRING)
 		{
-			tmp = malloc(sizeof(char *) * (strlen(last->word) + \
-			strlen(current->word) + 1));
-			//if NULL
-			tmp[0] = '\0';
-			strcpy(tmp, last->word); // creer strcpy
-			strcat(tmp, current->word); // creer strcat
+			tmp = ft_strjoin(current->word, last->word);
+			if (tmp == NULL)
+				return ;
 			free(last->word);
-			last->word = tmp;
-			// if (strcat(last->word, current->word) == NULL)
-			// 	exit(EXIT_FAILURE); //TODO error handling
 			free(current->word);
+			last->word = tmp;
 			return ;
 		}
 	}
