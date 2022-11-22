705ec98fd8eafa69e7ca139cabb2feeec3cf7dd5
tbousque <tbousque@student.42.fr>
2022-11-22 21:30:43 +0100

    verify if a cmd path is relative

--- path_finder/path_finder.c
+++ path_finder/path_finder.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 00:15:31 by tbousque          #+#    #+#             */
-/*   Updated: 2022/11/13 18:02:05 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/11/22 21:27:36 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ char	*find_exec(char *exec_name, char *path_array)
 
 	executable_path = NULL;
 	current_path = NULL;
-	if (exec_name == NULL || path_array == NULL)
+	if (exec_name == NULL || path_array == NULL || ft_strchr(exec_name, '/'))
 		return (NULL);
 	while (*path_array)
 	{
